23e0d343d94c86ce56c1c0d56ac90c3593dee6e2
Ibrahim Mercado <ahmedelgrande1@gmail.com>
2017-03-30 23:35:57 -0400

    creation of Taco Bells fake site

---
+++ SeniorTracker/personalinfo.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('#contact_form').bootstrapValidator({
+    	feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            first_name: {
+                validators: {
+                        stringLength: {
+                        min: 2,
+                    },
+                        notEmpty: {
+                        message: 'Please supply your first name'
+                    }
+                }
+            },
+            
